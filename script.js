9768fc00fe1c2c08fae6198c264a3a27afaf29b9
Patrick Akuagwu <patrick4akuagwu@yahoo.com>
2022-06-23 12:18:02 +0000

    JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName = prompt("Enter your name")
+let myHeight = prompt("Enter your Height")
+let myCountry = prompt("Enter your country")
+
+document.write("Name : " + myName + "<br>");
+document.write("Height : " + myHeight + "<br>");
+document.write("Country : " + myCountry);
